dfc7b931d63e802634ff8cc39aac792b338806e8
vpf26432 <laura.shemilt@rfi.ac.uk>
2023-11-02 10:57:15 +0000

    remove uneeded load image

--- pygame-templates/utils.py
+++ pygame-templates/utils.py
@@ -2,17 +2,11 @@ import pygame as pg
 from pathlib import Path
 
 
-def load_image(fpath):
-    """loads an image, prepares it for play"""
+def load_and_scale( image_path, scale_factor):
     try:
-        surface = pg.image.load(fpath)
+        image = pg.image.load(image_path)
     except pg.error:
         raise SystemExit(f'Could not load image "{file}" {pg.get_error()}')
-    return surface
-
-
-def load_and_scale( image_path, scale_factor):
-    image = pg.image.load(image_path)
     size = image.get_size()
     size = (size[0] * scale_factor, size[1] * scale_factor)
     image = pg.transform.scale(image, size)
