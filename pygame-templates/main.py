b6b10db275d530233107119e3b5276b3bfd57ca0
vpf26432 <laura.shemilt@rfi.ac.uk>
2023-11-06 10:45:32 +0000

    finished game and read me

--- pygame-templates/main.py
+++ pygame-templates/main.py
@@ -6,20 +6,25 @@ def main():
         """this function is called when the program starts.
         it initializes everything it needs, then runs in
         a loop until the function returns."""
-        # Initialize Everything
+        # Initialize Game
         pg.init()
+
+         # Initialize Arena
         arena = Arena(config="configs/arena_config.yml")
         arena.create_background()
         arena.display()
         pg.display.flip()
 
+        # Initialize Sprites
         imp = FranklinImp("configs/sprite_config.yml")
         tube = TestTube("configs/testtube_config.yml", arena.background)
-        allsprites = pg.sprite.Group(imp, tube)
+        allsprites = pg.sprite.Group(imp, tube) # You can create groups of sprites for updates
         tubes = pg.sprite.Group(tube)
+
+        #Intilize clock sets the frame update rate in the game
         clock = pg.time.Clock()
 
-
+        # The Game Loop
         going = True
         while going:
             clock.tick(60)
@@ -31,26 +36,27 @@ def main():
                     going = False
                 elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
                     going = False
+
                 # Move the sprite
                 elif event.type == pg.KEYDOWN and event.key == pg.K_LEFT:
                      imp.update(-1)
                 elif event.type == pg.KEYDOWN and event.key == pg.K_RIGHT:
                      imp.update(1)
                 
-                # Interactions
+                # Sprite Interactions
                 if pg.sprite.spritecollide(imp, tubes, 1):
                      tube.catch()
                      imp.jump()
                      tube.update()
                      game_over= SpriteConfig("configs/game_over_config.yml")
                      allsprites.add(game_over)
-        
+
+            # Update the characters with current state
             tube.update()
             arena.screen.blit(arena.background, (0, 0))
             allsprites.draw(arena.screen)
             
-            pg.display.flip()
-            
+            pg.display.flip()     
 
         pg.quit()
 
